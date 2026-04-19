ca4f8acf76fa2b852fd9440213e3226d55287db0
Vaizhnavi Selvaraj <selvarajvaizhnavi@gmail.com>
2026-04-19 22:40:04 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer({ setCurrentPage }: FooterProps) {
           <div>
             <div className="flex items-center mb-4">
               <img
-                src="/_VC_Sivraj_Bag.jpg.jpeg"
+                src="/bagpaks-logo.png"
                 alt="Bagpak Logo"
                 className="h-12 w-12 object-contain"
               />
